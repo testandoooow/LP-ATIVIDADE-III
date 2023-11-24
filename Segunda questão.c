6029bf630b0c6a6ef8327f39d9e7469f95f1ec74
Kaique.s <141377576+testandoooow@users.noreply.github.com>
2023-11-24 14:19:39 -0300

    Update and rename Untitled4.cpp to Segunda questão.c

--- Segunda questão.c
+++ Segunda questão.c
@@ -4,18 +4,18 @@
 
 int main(){
 	setlocale(LC_ALL, "portuguese");
-	float nota[3][3], media[3], soma = 0;
-	char nome[3][250];
-	int idade[3], i, j;
+	float nota[5][3], media[5], soma = 0;
+	char nome[5][250];
+	int idade[5], i, j;
 	
 	printf("Cadastro.\n");
-	for(i = 0; i < 3; i++){
+	for(i = 0; i < 5; i++){
 		printf("Aluno: ");
 		gets(nome[i]);
 		printf("Idade: ");
 		scanf("%d",&idade[i]);
 		for(j = 0; j < 3; j++){
-			printf("%d Nota: ",j + 1);
+			printf("%dª Nota: ",j + 1);
 			scanf("%f",&nota[i][j]);
 			soma += nota[i][j];
 		}
@@ -27,19 +27,18 @@ int main(){
 	system("cls");
 	
 	
-	for(i = 0; i < 3; i++){
-		printf("Aluno: %s\n",nome[i]);	
-		printf("Idade: %d\n",idade[i]);	
-			for(j = 0; j < 3; j++){
-			printf("%d Nota: %.1f\n",j + 1,nota[i][j]);
+	for(i = 0; i < 5; i++){
+		printf("Aluno: %s. Idade: %d\n",nome[i],idade[i]);	
+			for(j = 0; j < 5; j++){
+			printf("%dª Nota: %.1f\n",j + 1,nota[i][j]);
 			}
 			printf("Media: %.1f\n",media[i]);
 			if(media[i] >= 7){
 				printf("Aprovado!\n");
 			} else if(media[i] >= 5){
-				printf("Recupera��o!\n");
+				printf("Recuperação!\n");
 			} else {
-				printf("Reprovado!");
+				printf("Reprovado!\n");
 			}
 		}
 	return 0;
