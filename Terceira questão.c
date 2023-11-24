0c61e752b4b903369946bac5692c801ec47edf0b
Kaique.s <141377576+testandoooow@users.noreply.github.com>
2023-11-24 14:20:10 -0300

    Update and rename Untitled6.cpp to Terceira questão.c

--- Terceira questão.c
+++ Terceira questão.c
@@ -4,20 +4,21 @@
 
 int main(){
 	setlocale(LC_ALL, "portuguese");
-	int telefone[100], opcao, i, j, z = 0;
-	char nome[100][250];
+	int telefone[2000], opcao, i, j, z = 0;
+	char nome[2000][250];
 	
-	for(i = 0; i < 100; i++){
+	for(i = 0; i < 2000; i++){
 		printf("Escreva seu nome: ");
 		gets(nome[i]);
 		printf("Digite seu telefone: ");
 		scanf("%d",&telefone[i]);
-		printf("1 - Para adicionar um número. 2 -  Para mostrar os números cadastrados: ");
+		printf("1 - Para adicionar um nÃºmero. 2 -  Para mostrar os nÃºmeros cadastrados: ");
 		scanf("%d",&opcao);
 		z++;
 		fflush(stdin);
 		if(opcao == 2){
 			system("cls");
+            printf("Agenda.\n");
 			for(i = 0; i < z; i++){
 			printf("Nome: %s\n",nome[i]);
 			printf("Telefone: %d\n",telefone[i]);
